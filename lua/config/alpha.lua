a850454cdf57f3593a94d147da3512f85017c0d0
guankai1 <guankai1@corp.netease.com>
2022-12-05 17:57:05 +0800

    fix the config path to platform based and change telescope-fzf-native build setting

--- lua/config/alpha.lua
+++ lua/config/alpha.lua
@@ -1,40 +1,47 @@
 local ascii = {
-"                         .^!^                                           .!~:                        ",
-"                    ^!JPB&B!.                                            !&&BPJ!:                   ",
-"                ^?P#@@@@@G.                   :       :                   !@@@@@&BY!:               ",
-"             ^JB@@@@@@@@@7                   .#~     ?P                   .&@@@@@@@@&G?:            ",
-"          .7G@@@@@@@@@@@@#!                  ?@#^   ~@@^                 .5@@@@@@@@@@@@@G7.         ",
-"        .?#@@@@@@@@@@@@@@@@BY!^.             B@@&BBB&@@Y              :~Y&@@@@@@@@@@@@@@@@#?.       ",
-"       !#@@@@@@@@@@@@@@@@@@@@@@#G5Y?!~^:..  !@@@@@@@@@@#.   ..::^!7J5B&@@@@@@@@@@@@@@@@@@@@@B!      ",
-"     .5@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&&##B#@@@@@@@@@@@BBBB##&@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@Y     ",
-"    :B@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@5    ",
-"   .B@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@?   ",
-"   5@&#BGGPPPPPGGB&@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&BGGPP5PPPGBB#&#.  ",
-"   ^:..           .^!YB@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&57^.            .:^.  ",
-"                       ~G@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@Y.                      ",
-"                         P@@@#BGGGGB#@@@@@@@@@@@@@@@@@@@@@@@@@#BP5555PG#@@@P                        ",
-"                         :J!:.      .^!JG&@@@@@@@@@@@@@@@@#57^.        .:!5~                        ",
-"                                         :?G@@@@@@@@@@@@P!.                                         ",
-"                                            ~5@@@@@@@@5^                                            ",
-"                                              ^P@@@@G^                                              ",
-"                                                !#@?                                                ",
-"                                                 :^ ",
+  "                         .^!^                                           .!~:                        ",
+  "                    ^!JPB&B!.                                            !&&BPJ!:                   ",
+  "                ^?P#@@@@@G.                   :       :                   !@@@@@&BY!:               ",
+  "             ^JB@@@@@@@@@7                   .#~     ?P                   .&@@@@@@@@&G?:            ",
+  "          .7G@@@@@@@@@@@@#!                  ?@#^   ~@@^                 .5@@@@@@@@@@@@@G7.         ",
+  "        .?#@@@@@@@@@@@@@@@@BY!^.             B@@&BBB&@@Y              :~Y&@@@@@@@@@@@@@@@@#?.       ",
+  "       !#@@@@@@@@@@@@@@@@@@@@@@#G5Y?!~^:..  !@@@@@@@@@@#.   ..::^!7J5B&@@@@@@@@@@@@@@@@@@@@@B!      ",
+  "     .5@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&&##B#@@@@@@@@@@@BBBB##&@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@Y     ",
+  "    :B@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@5    ",
+  "   .B@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@?   ",
+  "   5@&#BGGPPPPPGGB&@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&BGGPP5PPPGBB#&#.  ",
+  "   ^:..           .^!YB@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&57^.            .:^.  ",
+  "                       ~G@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@Y.                      ",
+  "                         P@@@#BGGGGB#@@@@@@@@@@@@@@@@@@@@@@@@@#BP5555PG#@@@P                        ",
+  "                         :J!:.      .^!JG&@@@@@@@@@@@@@@@@#57^.        .:!5~                        ",
+  "                                         :?G@@@@@@@@@@@@P!.                                         ",
+  "                                            ~5@@@@@@@@5^                                            ",
+  "                                              ^P@@@@G^                                              ",
+  "                                                !#@?                                                ",
+  "                                                 :^ ",
 }
 
 local alpha = require("alpha")
 local dashboard = require("alpha.themes.dashboard")
-
+local setting_path = ""
+local has = vim.fn.has
+if (has("win32") or has("win64")) then
+  setting_path = "~/AppData/Local/nvim/"
+else
+  setting_path = "~/.config/nvim/"
+end
 -- Set header
 dashboard.section.header.val = ascii
 
 -- Set menu
 dashboard.section.buttons.val = {
-    dashboard.button( "e", "  > New file" ,    ":ene <BAR> startinsert<CR>"),
-    dashboard.button( "f", "  > Find file",    ":Telescope find_files hidden=true no_ignore=true<CR>"),
-    dashboard.button( "r", "  > Recent"   ,    ":Telescope oldfiles<CR>"),
-    dashboard.button( "s", "  > Settings" ,    ":Telescope find_files cwd=~/.config/nvim/ search_dirs=Ultisnips,lua,viml,init.vim<CR>"),
-    dashboard.button( "u", "  > Update Plugin",":PackerSync<CR>"), 
-    dashboard.button( "q", "  > Quit NVIM",    ":qa<CR>"),
+  dashboard.button("e", "  > New file", ":ene <BAR> startinsert<CR>"),
+  dashboard.button("f", "  > Find file", ":Telescope find_files hidden=true no_ignore=true<CR>"),
+  dashboard.button("r", "  > Recent", ":Telescope oldfiles<CR>"),
+  dashboard.button("s", "  > Settings",
+    ":Telescope find_files cwd=" .. setting_path .. " search_dirs=Ultisnips,lua,viml,init.vim<CR>"),
+  dashboard.button("u", "  > Update Plugin", ":PackerSync<CR>"),
+  dashboard.button("q", "  > Quit NVIM", ":qa<CR>"),
 }
 
 -- Set footer
@@ -50,7 +57,7 @@ dashboard.section.buttons.val = {
 --       }
 --   end)
 --   ```
--- local fortune = require("alpha.fortune") 
+-- local fortune = require("alpha.fortune")
 -- dashboard.section.footer.val = fortune()
 
 -- Send config to alpha
--- lua/plugin.lua
+++ lua/plugin.lua
@@ -126,7 +126,7 @@ return require('packer').startup({ function(use)
       'nvim-lua/plenary.nvim',
       {
         "nvim-telescope/telescope-fzf-native.nvim",
-        run = "make",
+        run = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
       },
     },
     config = function()
