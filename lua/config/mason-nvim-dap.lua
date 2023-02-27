f0c20c9cb987655f1a7b70ed6d33a6891a1c9b0f
guankai <heliar_mk@163.com>
2023-02-27 22:25:18 +0800

    empty the setup_handles for mason-nvim-dap

--- lua/config/mason-nvim-dap.lua
+++ lua/config/mason-nvim-dap.lua
@@ -1,47 +1,8 @@
 local dap = require('dap')
 
 require('mason-nvim-dap').setup({
+  ensure_installed = { "bash", "python", "codelldb" },
   automatic_setup = true,
 })
 
-require 'mason-nvim-dap'.setup_handlers {
-  function(source_name)
-    -- all sources with no handler get passed here
-
-
-    -- Keep original functionality of `automatic_setup = true`
-    require('mason-nvim-dap.automatic_setup')(source_name)
-  end,
-  python = function(source_name)
-    dap.adapters.python = {
-      type = "executable",
-      command = "/usr/bin/python3",
-      args = {
-        "-m",
-        "debugpy.adapter",
-      },
-    }
-
-    dap.configurations.python = {
-      {
-        type = "python",
-        request = "launch",
-        name = "Launch file",
-        program = "${file}", -- This configuration will launch the current file if used.
-      },
-    }
-
-    dap.adapters.codelldb = {
-      type = 'server',
-      port = "${port}",
-      executable = {
-        -- CHANGE THIS to your path!
-        command = '/absolute/path/to/codelldb/extension/adapter/codelldb',
-        args = { "--port", "${port}" },
-
-        -- On windows you may have to uncomment this:
-        -- detached = false,
-      }
-    }
-  end,
-}
+require 'mason-nvim-dap'.setup_handlers {}
