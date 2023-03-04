51672c8b7461fc70e3ba79ff1e2a33a80bb51d75
guankai <heliar_mk@163.com>
2023-03-04 13:08:39 +0800

    migrate autoinstall lazy.nvim from init.lua to plugin.lua

--- init.lua
+++ init.lua
@@ -1,26 +1,10 @@
--- 自动安装lazy.nvim
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
-end
-
-vim.opt.rtp:prepend(lazypath)
-
+-- 插件设置
+require('plugin')
 -- 编辑器设置
 require('editor')
-
 -- 快捷键映射
 require('keybindings')
 
--- 插件设置
-require('plugin')
 
 vim.cmd [[colorscheme catppuccin-latte]]
 
--- lua/plugin.lua
+++ lua/plugin.lua
@@ -1,6 +1,18 @@
-local lazy = require("lazy")
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
 
-lazy.setup({
+return require('lazy').setup({
   {
     'williamboman/mason.nvim',
     config = function()
