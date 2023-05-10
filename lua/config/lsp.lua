796474ba7f70e15126a561340662d942f5588be4
guankai01 <heliar_mk@163.com>
2023-05-10 09:42:41 +0800

    add clangd in lspconfig

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -13,3 +13,4 @@ lspconfig.pyright.setup { on_attach = on_attach }
 lspconfig.lua_ls.setup { on_attach = on_attach }
 lspconfig.rust_analyzer.setup { on_attach = on_attach }
 lspconfig.marksman.setup { on_attach = on_attach }
+lspconfig.clangd.setup { on_attach = on_attach }
