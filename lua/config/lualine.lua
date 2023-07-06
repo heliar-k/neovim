f76a19c463a978c5c489de072b8c2c04088e9fe3
guankai <heliar_mk@163.com>
2023-07-06 15:21:35 +0800

    move datetime to the rightside and change its format in lualine

--- lua/config/lualine.lua
+++ lua/config/lualine.lua
@@ -1,16 +1,16 @@
 require('lualine').setup({
   sections = {
-    lualine_a = {
-      {
-        'datetime',
-        -- options: default, us, uk, iso, or your own format string ("%H:%M", etc..)
-        style = 'us'
-      }
-    },
     lualine_c = {
       ...,
       'lsp_progress',
-    }
+    },
+    lualine_z = {
+      {
+        'datetime',
+        -- options: default, us, uk, iso, or your own format string ("%H:%M", etc..)
+        style = '%Y-%m-%d|%H:%M:%S',
+      }
+    },
   },
   options = {
     theme = "auto"
