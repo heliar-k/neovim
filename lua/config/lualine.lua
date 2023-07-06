01cbed58ac822ad80ff760d6e384b83e1e335253
guankai <heliar_mk@163.com>
2023-07-06 15:16:47 +0800

    add datatime and disable filetype to lualine

--- lua/config/lualine.lua
+++ lua/config/lualine.lua
@@ -1,11 +1,28 @@
 require('lualine').setup({
   sections = {
+    lualine_a = {
+      {
+        'datetime',
+        -- options: default, us, uk, iso, or your own format string ("%H:%M", etc..)
+        style = 'us'
+      }
+    },
     lualine_c = {
       ...,
-      'lsp_progress'
+      'lsp_progress',
     }
   },
   options = {
     theme = "auto"
-  }
+  },
+  disabled_filetypes = {
+    statusline = {
+      "alpha",
+      "NvimTree"
+    },
+    winbar = {
+      "alpha",
+      "NvimTree"
+    },
+  },
 })
