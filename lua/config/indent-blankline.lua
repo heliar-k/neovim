6bf7f4bd4b9294f5e219cac9540382eac0357a4d
guankai <heliar_mk@163.com>
2023-07-06 14:59:02 +0800

    add empty filetype to filetype_exclude in indent_blankline

--- lua/config/indent-blankline.lua
+++ lua/config/indent-blankline.lua
@@ -3,17 +3,19 @@ vim.opt.listchars:append "space:⋅"
 vim.opt.listchars:append "eol:↴"
 
 require("indent_blankline").setup {
-    space_char_blankline = " ",
-    show_current_context = true,
-    show_current_context_start = true,
-    filetype_exclude = {
-      "help",
-      "terminal",
-      "dashboard",
-      "packer",
-      "lspinfo",
-      "TelescopePrompt",
-      "TelescopeResults",
-      "NvimTree",
-  }
+  space_char_blankline = " ",
+  show_current_context = true,
+  show_current_context_start = true,
+  filetype_exclude = {
+    "alpha",
+    "help",
+    "terminal",
+    "dashboard",
+    "packer",
+    "lspinfo",
+    "TelescopePrompt",
+    "TelescopeResults",
+    "NvimTree",
+    "",
+  },
 }
