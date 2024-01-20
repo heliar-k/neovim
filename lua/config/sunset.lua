f04c6f282fc15d2f5885d0462a292bfc56f105f6
guankai <heliar_mk@163.com>
2024-01-20 17:34:43 +0800

    feat(theme): change color from catppuccin to github

--- lua/config/sunset.lua
+++ lua/config/sunset.lua
@@ -8,12 +8,12 @@ function M.setup()
     sunrise_override = "07:00", -- accepts a time in the form "HH:MM" which will override the sunrise time
     sunset_override = "17:00",  -- accepts a time in the form "HH:MM" which will override the sunset time
     day_callback = function()
-      vim.cmd("colorscheme catppuccin-latte")
-      -- vim.cmd("colorscheme github_light")
+      -- vim.cmd("colorscheme catppuccin-latte")
+      vim.cmd("colorscheme github_light")
     end, -- function that is called when day begins
     night_callback = function()
-      vim.cmd("colorscheme catppuccin-macchiato")
-      -- vim.cmd("colorscheme github_dark")
+      -- vim.cmd("colorscheme catppuccin-macchiato")
+      vim.cmd("colorscheme github_dark_dimmed")
     end,                     -- function that is called when night begins
     update_interval = 60000, -- how frequently to check for sunrise/sunset changes in milliseconds
     time_format = "%H:%M",   -- sun time formatting using os.date https://www.lua.org/pil/22.1.html
