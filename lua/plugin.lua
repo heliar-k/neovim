435f07a03b1622fcd343136b82df936e411ff5f0
heliar_mk <heliar_mk@163.com>
2024-07-28 00:28:50 +0800

    feat(neo-tree): add cmd to it

--- lua/plugin.lua
+++ lua/plugin.lua
@@ -174,6 +174,8 @@ return require("lazy").setup(
     {
       "nvim-neo-tree/neo-tree.nvim",
       branch = "v3.x",
+      lazy=true,
+      cmd = { "Neotree" },
       dependencies = {
         "nvim-lua/plenary.nvim",
         "nvim-tree/nvim-web-devicons",
