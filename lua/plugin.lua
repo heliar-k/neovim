ff872c11d2293dd4d6838172bd120871748760d3
heliar_mk <heliar_mk@163.com>
2024-07-27 23:37:43 +0800

    feat(lazy): use vim.uv if it is available

--- lua/plugin.lua
+++ lua/plugin.lua
@@ -1,6 +1,9 @@
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
+local uv = vim.uv
+  ---@diagnostic disable-next-line: deprecated
+  or vim.loop
+if not uv.fs_stat(lazypath) then
   vim.fn.system({
     "git",
     "clone",
