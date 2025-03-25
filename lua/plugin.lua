616c3bbb281f584f1bcf0402869419e835794d29
heliar-k <heliar_mk@163.com>
2025-03-25 17:35:00 +0800

    feat(plugin.lua):remove ctags auto-gen

--- lua/plugin.lua
+++ lua/plugin.lua
@@ -78,10 +78,10 @@ return require("lazy").setup(
       end,
     },
     -- Ctags auto generation
-    {
-      "ludovicchabant/vim-gutentags",
-      branch = "master",
-    },
+    -- {
+    --   "ludovicchabant/vim-gutentags",
+    --   branch = "master",
+    -- },
     -- Displaying errors/warnings in a window
     {
       "folke/trouble.nvim",
