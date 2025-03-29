a3ded928069d400ea0703ce60b16b8460047e9ee
关凯 <guankai@guankaideMac-mini.local>
2025-03-29 12:07:48 +0800

    feat(mason): add protobuf related lsp and linter, remove prettier

--- lua/config/mason.lua
+++ lua/config/mason.lua
@@ -22,16 +22,19 @@ require("mason-tool-installer").setup({
     "rust-analyzer", -- lsp for rust
     "yaml-language-server", -- lsp for yaml
     "taplo", -- lsp for toml
+    "protols", -- lsp for protobuf
     -- dap
     "codelldb", -- debugger for c/c++
     "cpptools", -- debugger for c/c++
     "debugpy", -- debugger for python
+    -- linter
+    "cmakelang", -- linter for cmake
+    "protolint", -- linter for protobuf
     -- formatter
     "black", -- formatter for python
     "clang-format", -- formatter for c/c++
     "cmakelang", -- formatter for cmake
     "isort", -- import optimizer for python
-    "prettier", -- formatter for javascript/typescript
     "prettierd", -- formatter for javascript/typescript
     "shfmt", -- formatter for shell scripts
     "stylua", -- formatter for lua
