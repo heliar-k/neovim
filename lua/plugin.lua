5adea7db70da60b68980da2b7c3230cfa1332529
heliar <heliar_mk@163.com>
2026-01-10 20:28:19 +0800

    feat(plugin):delete comment in opencode opts

--- lua/plugin.lua
+++ lua/plugin.lua
@@ -345,10 +345,7 @@ return require("lazy").setup(
         { "folke/snacks.nvim", opts = { input = {}, picker = {}, terminal = {} } },
       },
       config = function()
-        ---@type opencode.Opts
-        vim.g.opencode_opts = {
-          -- Your configuration, if any — see `lua/opencode/config.lua`, or "goto definition".
-        }
+        vim.g.opencode_opts = {}
         -- Required for `opts.events.reload`.
         vim.o.autoread = true
       end,
